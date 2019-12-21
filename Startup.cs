06e45db0d52fa969547ad1055bd65f588d0526ac
Jasander97 <58708081+Jasander97@users.noreply.github.com>
2019-12-21 21:12:58 +0000

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace HD_Decisions_Case_Study
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -42,7 +42,7 @@ namespace HD_Decisions_Case_Study
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
