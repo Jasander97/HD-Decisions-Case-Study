743fa54ce6137fc45eb736bed4c7dd9d4515e39a
Jasander97 <58708081+Jasander97@users.noreply.github.com>
2019-12-21 21:29:43 +0000

    Add files via upload

---
+++ Data/FormDataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HD_Decisions_Case_Study.Models;
+
+namespace HD_Decisions_Case_Study.Data
+{
+    public class HD_Decisions_Case_StudyContext : DbContext
+    {
+        public HD_Decisions_Case_StudyContext(DbContextOptions<HD_Decisions_Case_StudyContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<FormData> FormData { get; set; }
+    }
+}
